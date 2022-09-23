ee418e5d55dc029c81d9f22566be75e2a25b2692
Jackson Almeida <jacksonnalmeida30@gmail.com>
2022-09-23 14:57:22 -0400

    worker i++

--- hr-worker/src/main/java/com/sunflower/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/sunflower/hrworker/resources/WorkerResource.java
@@ -1,15 +1,23 @@
 package com.sunflower.hrworker.resources;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sunflower.hrworker.DTO.WorkerDTO;
+import com.sunflower.hrworker.entities.Worker;
 import com.sunflower.hrworker.services.WorkerService;
 
 @RestController
@@ -19,6 +27,15 @@ public class WorkerResource {
 	@Autowired
 	private WorkerService workerService;
 
+	@PostMapping(value = "/newworker")
+	public ResponseEntity<Void> save(@RequestBody WorkerDTO obj){
+		Worker worker = new Worker();
+		BeanUtils.copyProperties(obj, worker);
+		workerService.insert(worker);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
 	@GetMapping
 	public ResponseEntity<List<WorkerDTO>> findAll() {
 		List<WorkerDTO> dtoList = workerService.findAll();
@@ -31,4 +48,15 @@ public class WorkerResource {
 		return ResponseEntity.ok().body(dto);
 	}
 
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable long id){
+		workerService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/updateworker/{id}")
+	public ResponseEntity<Void> update(long id, Worker obj){
+		obj = workerService.update(id, obj);
+		return ResponseEntity.ok().build();
+	}
 }
--- hr-worker/src/main/java/com/sunflower/hrworker/services/WorkerService.java
+++ hr-worker/src/main/java/com/sunflower/hrworker/services/WorkerService.java
@@ -4,12 +4,17 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.sunflower.hrworker.DTO.WorkerDTO;
 import com.sunflower.hrworker.entities.Worker;
 import com.sunflower.hrworker.repositories.WorkerRepository;
+import com.sunflower.hrworker.services.exceptions.DatabaseException;
 import com.sunflower.hrworker.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -17,6 +22,10 @@ public class WorkerService {
 
 	@Autowired
 	private WorkerRepository workerRepository;
+
+	public Worker insert(Worker obj) {
+		return workerRepository.save(obj);
+	}
 
 	public List<WorkerDTO> findAll() {
 		List<Worker> list = workerRepository.findAll();
@@ -28,11 +37,37 @@ public class WorkerService {
 		Optional<Worker> obj = workerRepository.findById(id);
 		if (obj.isPresent()) {
 			return obj.stream().map(x -> new WorkerDTO(x)).findAny().get();
-		}
-		else {
+		} else {
 			Optional<WorkerDTO> dto = Optional.empty();
 			return dto.orElseThrow(() -> new ResourceNotFoundException(id));
 		}
+	}
+
+	public void delete(long id) {
+
+		try {
+			workerRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+
+	}
+
+	public Worker update(long id, Worker obj) {
+		try {
+			Worker entity = workerRepository.getReferenceById(id);
+			updateData(entity, obj);
+			return workerRepository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+
+	private void updateData(Worker entity, Worker obj) {
+		entity.setName(obj.getName());
+		entity.setDailyIncome(obj.getDailyIncome());
 	}
 
 }
